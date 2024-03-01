a48dd1f27c09e045ec54fabb3a7bb7f0805cd313
Bill Sideris <bill88t@feline.gr>
2024-03-01 21:31:17 +0200

    Fix strap

--- files/strap.py
+++ files/strap.py
@@ -1,4 +1,4 @@
 for i in ["camera.lja", "camera.py"]:
     shutil.copy(i, path.join(root, "bin", i))
 
-shutil.copy("camera.txt", path.join(root, "bin", "camera.txt"))
+shutil.copy("camera.txt", path.join(root, "usr/share/help", "camera.txt"))
