fabba2c1a015151e95c314931c056c6f95ff4763
Bill Sideris <bill88t@feline.gr>
2024-05-29 03:03:06 +0300

    copyfile

--- files/strap.py
+++ files/strap.py
@@ -1,4 +1,4 @@
 for i in ["camera.lja", "camera.py"]:
-    shutil.copy(i, path.join(root, "bin", i))
+    shutil.copyfile(i, path.join(root, "bin", i))
 
-shutil.copy("camera.txt", path.join(root, "usr/share/help", "camera.txt"))
+shutil.copyfile("camera.txt", path.join(root, "usr/share/help", "camera.txt"))
